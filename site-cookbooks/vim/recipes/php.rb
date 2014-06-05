2523b7a825b0843fafe5015cbe5cafc3c5c4464a
k_ohhara <k_ohhara@corp.allabout.co.jp>
2014-06-05 11:29:12 +0200

    auto neobundleinstall

--- site-cookbooks/vim/recipes/php.rb
+++ site-cookbooks/vim/recipes/php.rb
@@ -1,6 +1,11 @@
 cookbook_file "/root/.vimrc" do
   source "vimrc_php"
 end
+bash "neobundle install" do
+  code <<-EOC
+    vim +":NeoBundleInstall" +:q
+  EOC
+end
 directory '/root/.vim/syntax' do
   action :create
 end
--- site-cookbooks/vim/recipes/ruby.rb
+++ site-cookbooks/vim/recipes/ruby.rb
@@ -1,3 +1,8 @@
 cookbook_file "/root/.vimrc" do
   source "vimrc_ruby"
 end
+bash "neobundle install" do
+  code <<-EOC
+    vim +":NeoBundleInstall" +:q
+  EOC
+end
