83e40297bf8c48a816ae9ca0137782a7db7805c8
k_ohhara <luka.and.evans@gmail.com>
2014-09-08 07:01:03 +0100

    インデント修正

--- site-cookbooks/php/recipes/default.rb
+++ site-cookbooks/php/recipes/default.rb
@@ -7,8 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 %w{php php-devel php-mbstring php-mcrypt php-mysql php-pecl-xdebug}.each do |pkg|
-    package pkg do
-        options "--enablerepo=remi-php55"
-        action :install
-    end
+  package pkg do
+    options "--enablerepo=remi-php55"
+    action :install
+  end
 end
