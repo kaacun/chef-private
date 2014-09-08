5ad649865586370bbdf067ca675257f94ec8d302
k_ohhara <luka.and.evans@gmail.com>
2014-09-08 07:03:49 +0100

    tool追加

---
+++ site-cookbooks/tools/attributes/default.rb
@@ -0,0 +1,19 @@
+# util tools list to install
+# add to array tool if you want more
+# -----------------------------
+default['tools']['packages'] = [
+  'wget',
+  'rsync',
+  'telnet',
+  'dstat',
+  'man',
+  'tcpdump',
+  'nkf',
+  'nmap',
+  'expect',
+  'ngrep',
+  'iftop',
+  'net-snmp-utils',
+  'sysstat',
+  'tree'
+]
--- site-cookbooks/tools/recipes/default.rb
+++ site-cookbooks/tools/recipes/default.rb
@@ -6,3 +6,21 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+# # action #####################################
+# 各種ユーティリティ系のインストール
+# 
+#
+log '== start tools =================================='
+
+log '## install/upgrade tools #####################'
+node['tools']['packages'].each do |pkg|
+  package pkg do
+    action :install
+  end
+end
+
+template '/etc/profile.d/history.sh' do
+  source 'etc/profile.d/history.sh.erb'
+end
+
+log '== tools, done =================================='
