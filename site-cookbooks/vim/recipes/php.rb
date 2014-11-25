c02bd659c287f1b327feab427192bc2659748a8b
k_ohhara <k_ohhara@corp.allabout.co.jp>
2014-11-25 17:59:28 +0900

    neobundleinstallこけるので一旦コメントアウト

--- site-cookbooks/vim/recipes/php.rb
+++ site-cookbooks/vim/recipes/php.rb
@@ -1,11 +1,13 @@
 cookbook_file "/root/.vimrc" do
   source "vimrc_php"
 end
+=begin
 bash "neobundle install" do
   code <<-EOC
     vim +":NeoBundleInstall" +:q
   EOC
 end
+=end
 directory '/root/.vim/syntax' do
   action :create
 end
